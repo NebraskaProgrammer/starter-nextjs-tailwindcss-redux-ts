d734eacbd6dc5b7eb00ea9ad8789dc101a5bc427
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-03-31 02:44:23 +0000

    feat: upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
@@ -10,9 +13,6 @@ module.exports = {
         cyan: colors.cyan,
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 };
